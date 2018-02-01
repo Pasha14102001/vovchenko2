0a0e9c39d6ca1695280e820617f442ea100b96af
student <student@tsn-elector.ttknn.net>
2018-02-01 14:27:10 +0300

    create 3 shade

---
+++ vovchenko navigator/vovchenko navigator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vovchenko navigator
+//
+//  Created by student on 01.02.2018.
+//  Copyright © 2018 Zheleznov Daniil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
